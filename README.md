7d46e886ca09b1d3f9a538d47a06168cbd9a133a
SENDA Keijiro <senda.keijiro@gmail.com>
2016-02-17 23:45:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+React-Chat
+====
+
+Settings
+----
+```
+npm install -g gulp
+gulp watch
+gulp
+ruby server.rb
+```
+
+Now access to http://localhost:3000
---
+++ src/main.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Chat = require('./chat.jsx');
+ReactDOM.render(
+  <Chat/>,
+  document.getElementById('content')
+);
